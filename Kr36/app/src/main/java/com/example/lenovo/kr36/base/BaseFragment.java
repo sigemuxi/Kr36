4c40356d3b33ee7d9a4034d14f3589cc0d6f6586
sigemuxi <463667708@qq.com>
2016-05-11 07:36:24 +0800

    no message

---
+++ Kr36/app/src/main/java/com/example/lenovo/kr36/base/BaseFragment.java
@@ -0,0 +1,7 @@
+package com.example.lenovo.kr36.base;
+
+/**
+ * Created by lenovo on 2016/5/10.
+ */
+public class BaseFragment {
+}
---
+++ Kr36/app/src/main/java/com/example/lenovo/kr36/bean.java
@@ -0,0 +1,7 @@
+package com.example.lenovo.kr36;
+
+/**
+ * Created by lenovo on 2016/5/10.
+ */
+public class bean {
+}
